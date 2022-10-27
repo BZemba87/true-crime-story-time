68759f13fe6467009a17c9a0e49c51b88a14236f
BZemba87 <zembabernadette@gmail.com>
2022-10-27 20:48:14 +0000

    create views for front end home page

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', )
+    search = ['title', 'content']
+
+
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
