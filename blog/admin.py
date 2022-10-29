5e886051057b075bcc8bc23b6d87fa6c6989f95e
BZemba87 <zembabernadette@gmail.com>
2022-10-29 12:17:19 +0000

    add post detail view to view content of blog post

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status', )
+    summernote_fields = ('content')
     search = ['title', 'content']
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from .models import Post 
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.views import generic, View
 
-# Create your views here.
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -10,7 +10,20 @@ class PostList(generic.ListView):
 
 
 class DetailView(generic.DetailView):
-    model = Post
-    template_name = 'post_detail.html'
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "liked": liked
+            },
+        )
     
 
