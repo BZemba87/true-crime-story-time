9493a094ffcc64cb29443c913acbbc377ed9c364
BZemba87 <zembabernadette@gmail.com>
2022-10-30 11:19:11 +0000

    add like buttons and views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from .models import Post
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 
 
 class PostList(generic.ListView):
@@ -25,5 +26,20 @@ class DetailView(generic.DetailView):
                 "liked": liked
             },
         )
+
+
+class PostLike(View):
+    def post(self, request, slug):
+           post = get_object_or_404(Post, slug=slug)
+           if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+           else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+
+
     
 
